5a633864246a20a2ccd40f8d902f7d01da900b81
ST10029256 <dylanmiller424@gmail.com>
2023-04-27 17:05:27 +0200

    Added music in program class , where I saved the music file in the music folder

--- PROG6221_POE_ST10029256/Classes/Program.cs
+++ PROG6221_POE_ST10029256/Classes/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace PROG6221_POE_ST10029256
 {
@@ -10,9 +12,19 @@ namespace PROG6221_POE_ST10029256
     {
         static void Main(string[] args)
         {
+            PlaySong("Sunrise-_Original-Mix_.wav");
             Worker_class worker = new Worker_class();
             worker.MainMenu();
     
+        }
+
+        private static void PlaySong(string song)
+        {
+
+            SoundPlayer musicPlayer = new SoundPlayer();
+            musicPlayer.SoundLocation = song;
+            musicPlayer.Play();
+            
         }
     }
 }
